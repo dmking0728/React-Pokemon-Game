58171aee8df9c2de31e8082bacb46685e2ce0324
daniel king <danielking@daniels-MacBook-Pro.local>
2019-06-15 12:05:31 -0400

    Updates

---
+++ precache-manifest.dd9c5bcd1f09df71a58738089c337884.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "645a1ea9f32f0dde22f2e1c4aff0bf93",
+    "url": "/React-Pokemon-Game/index.html"
+  },
+  {
+    "revision": "1ef65a814479d7f6937b",
+    "url": "/React-Pokemon-Game/static/css/main.ab4b0a2e.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/React-Pokemon-Game/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "1ef65a814479d7f6937b",
+    "url": "/React-Pokemon-Game/static/js/main.4bc22383.chunk.js"
+  },
+  {
+    "revision": "41c0118ba6838d0eeb8a",
+    "url": "/React-Pokemon-Game/static/js/runtime~main.8a5b78b2.js"
+  }
+]);
